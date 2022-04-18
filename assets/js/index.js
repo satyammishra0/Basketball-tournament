47f17c32223756dca643a84b90e3e0803946d5e5
satyam mishra <satyam.growupnext@gmail.com>
2022-04-18 12:18:25 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+var myfun = function() {
+    var header = document.querySelector('.header');
+    var mobile_header = document.querySelector('.mobile-header');
+    var oncall = document.querySelector('#oncall');
+
+    mobile_header.style.display = 'none';
+    oncall.style.display = 'block';
+    oncall.style.backgroundColor = '#d5c2e0';
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+this is the my client work made as per guided by DOCUMENTATION provided by HITESH CHAUDHARY (https://web.learncodeonline.in/)
+
+the website URL is ('https://basketball-tournament.000webhostapp.com/')
